20a2ac8919743c0c1dd08e0df17785aae18564ae
Eduardo de Paula <eduardoibarr56@gmail.com>
2024-10-07 14:29:17 -0300

    refactor: add cnpj mask and change styles from login, register and recover password pages

--- app/src/main/java/com/pi/supplybridge/MainActivity.kt
+++ app/src/main/java/com/pi/supplybridge/MainActivity.kt
@@ -10,9 +10,11 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -29,7 +31,6 @@ import androidx.compose.ui.unit.sp
 import com.pi.supplybridge.ui.theme.SupplyBridgeTheme
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
-import com.pi.supplybridge.R
 import com.pi.supplybridge.activities.DashboardActivity
 import com.pi.supplybridge.activities.ForgotPasswordActivity
 import com.pi.supplybridge.activities.HomeActivity
@@ -74,27 +75,43 @@ fun LoginScreen(
     onForgotPasswordClick: () -> Unit
 ) {
     val context = LocalContext.current
+    var email by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+    val auth: FirebaseAuth = FirebaseAuth.getInstance()
+
     Column(
-        modifier = Modifier.fillMaxSize(),
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp), // Adiciona padding
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        var email by remember { mutableStateOf("") }
-        var password by remember { mutableStateOf("") }
-        val auth: FirebaseAuth = FirebaseAuth.getInstance()
-
         Image(
-            modifier = Modifier.size(150.dp),
+            modifier = Modifier.size(140.dp),
             painter = painterResource(id = R.drawable.logo),
             contentDescription = stringResource(id = R.string.app_name)
         )
 
-        Spacer(modifier = Modifier.size(16.dp))
+        Spacer(modifier = Modifier.size(32.dp))
+
+        Text(
+            text = "Bem-vindo de volta!",
+            style = TextStyle(
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Bold
+            ),
+            modifier = Modifier.padding(vertical = 16.dp)
+        )
 
         OutlinedTextField(
             value = email,
             onValueChange = { email = it },
-            label = { Text("Email") }
+            label = { Text("E-mail") },
+            modifier = Modifier.fillMaxWidth(),
+            colors = TextFieldDefaults.colors(
+                focusedContainerColor = Color.LightGray,
+                unfocusedContainerColor = Color.White
+            )
         )
 
         Spacer(modifier = Modifier.size(16.dp))
@@ -103,10 +120,15 @@ fun LoginScreen(
             value = password,
             onValueChange = { password = it },
             label = { Text("Senha") },
-            visualTransformation = PasswordVisualTransformation()
+            visualTransformation = PasswordVisualTransformation(),
+            modifier = Modifier.fillMaxWidth(),
+            colors = TextFieldDefaults.colors(
+                focusedContainerColor = Color.LightGray,
+                unfocusedContainerColor = Color.White
+            )
         )
 
-        Spacer(modifier = Modifier.size(16.dp))
+        Spacer(modifier = Modifier.size(24.dp))
 
         Text(
             text = "Esqueceu a senha?",
@@ -120,44 +142,50 @@ fun LoginScreen(
                 .padding(8.dp)
         )
 
-        Spacer(modifier = Modifier.size(16.dp))
+        Spacer(modifier = Modifier.size(24.dp))
 
-        Button(onClick = {
-            auth.signInWithEmailAndPassword(email, password)
-                .addOnCompleteListener { task ->
-                    if (task.isSuccessful) {
-                        val userId = auth.currentUser?.uid
-                        if (userId != null) {
-                            val db = FirebaseFirestore.getInstance()
-                            db.collection("users").document(userId)
-                                .get()
-                                .addOnSuccessListener { document ->
-                                    if (document != null) {
-                                        val tipoUsuario = document.getString("userType")
-                                        if (tipoUsuario == "fornecedor") {
-                                            val intent = Intent(context, DashboardActivity::class.java)
+        Button(
+            onClick = {
+                auth.signInWithEmailAndPassword(email, password)
+                    .addOnCompleteListener { task ->
+                        if (task.isSuccessful) {
+                            val userId = auth.currentUser?.uid
+                            if (userId != null) {
+                                val db = FirebaseFirestore.getInstance()
+                                db.collection("users").document(userId)
+                                    .get()
+                                    .addOnSuccessListener { document ->
+                                        if (document != null) {
+                                            val tipoUsuario = document.getString("userType")
+                                            val intent = if (tipoUsuario == "fornecedor") {
+                                                Intent(context, DashboardActivity::class.java)
+                                            } else {
+                                                Intent(context, NewOrderActivity::class.java)
+                                            }
                                             context.startActivity(intent)
-                                        } else if (tipoUsuario == "loja") {
-                                            val intent = Intent(context, NewOrderActivity::class.java)
-                                            context.startActivity(intent)
+                                        } else {
+                                            Toast.makeText(context, "Erro ao buscar informações do usuário", Toast.LENGTH_SHORT).show()
                                         }
-                                    } else {
-                                        Toast.makeText(context, "Erro ao buscar informações do usuário", Toast.LENGTH_SHORT).show()
                                     }
-                                }
-                                .addOnFailureListener {
-                                    Toast.makeText(context, "Erro ao buscar dados do usuário", Toast.LENGTH_SHORT).show()
-                                }
+                                    .addOnFailureListener {
+                                        Toast.makeText(context, "Erro ao buscar dados do usuário", Toast.LENGTH_SHORT).show()
+                                    }
+                            }
+                        } else {
+                            Toast.makeText(context, "Erro ao fazer o login: ${task.exception?.message}", Toast.LENGTH_SHORT).show()
                         }
-                    } else {
-                        Toast.makeText(context, "Erro ao fazer o login: ${task.exception?.message}", Toast.LENGTH_SHORT).show()
                     }
-                }
-        }) {
+            },
+            modifier = Modifier.fillMaxWidth(),
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color.Blue,
+                contentColor = Color.White
+            )
+        ) {
             Text("Entrar")
         }
 
-        Spacer(modifier = Modifier.size(16.dp))
+        Spacer(modifier = Modifier.size(24.dp))
 
         Text(
             text = "Não possui uma conta? Registre-se",
@@ -172,6 +200,7 @@ fun LoginScreen(
         )
     }
 }
+
 
 
 @Preview(showBackground = true)
--- app/src/main/java/com/pi/supplybridge/activities/ForgotPasswordActivity.kt
+++ app/src/main/java/com/pi/supplybridge/activities/ForgotPasswordActivity.kt
@@ -13,9 +13,11 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -23,8 +25,12 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.google.firebase.auth.FirebaseAuth
 import com.pi.supplybridge.ui.theme.SupplyBridgeTheme
 
@@ -47,34 +53,56 @@ fun ForgotPasswordScreen(onBackClick: () -> Unit) {
     var email by remember { mutableStateOf("") }
 
     Column(
-        modifier = Modifier.fillMaxSize(),
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp), // Adiciona padding
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
+        Text(
+            text = "Recuperar Senha",
+            style = TextStyle(
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Bold
+            ),
+            modifier = Modifier.padding(vertical = 16.dp)
+        )
+
         OutlinedTextField(
             value = email,
             onValueChange = { email = it },
-            label = { Text("Email") },
-            modifier = Modifier.fillMaxWidth().padding(16.dp)
+            label = { Text("E-mail") },
+            modifier = Modifier.fillMaxWidth(),
+            colors = TextFieldDefaults.colors(
+                focusedContainerColor = Color.LightGray,
+                unfocusedContainerColor = Color.White
+            )
         )
 
         Spacer(modifier = Modifier.size(16.dp))
 
-        Button(onClick = {
-            if (email.isNotEmpty() && android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
-                FirebaseAuth.getInstance().sendPasswordResetEmail(email)
-                    .addOnCompleteListener { task ->
-                        if (task.isSuccessful) {
-                            Toast.makeText(context, "E-mail de recuperação enviado", Toast.LENGTH_SHORT).show()
-                            onBackClick()
-                        } else {
-                            Toast.makeText(context, "Erro ao enviar e-mail de recuperação", Toast.LENGTH_SHORT).show()
+        Button(
+            onClick = {
+                if (email.isNotEmpty() && android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+                    FirebaseAuth.getInstance().sendPasswordResetEmail(email)
+                        .addOnCompleteListener { task ->
+                            if (task.isSuccessful) {
+                                Toast.makeText(context, "E-mail de recuperação enviado", Toast.LENGTH_SHORT).show()
+                                onBackClick()
+                            } else {
+                                Toast.makeText(context, "Erro ao enviar e-mail de recuperação", Toast.LENGTH_SHORT).show()
+                            }
                         }
-                    }
-            } else {
-                Toast.makeText(context, "Digite um e-mail válido", Toast.LENGTH_SHORT).show()
-            }
-        }) {
+                } else {
+                    Toast.makeText(context, "Digite um e-mail válido", Toast.LENGTH_SHORT).show()
+                }
+            },
+            modifier = Modifier.fillMaxWidth(),
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color.Blue,
+                contentColor = Color.White
+            )
+        ) {
             Text("Recuperar Senha")
         }
 
@@ -82,7 +110,14 @@ fun ForgotPasswordScreen(onBackClick: () -> Unit) {
 
         Text(
             text = "Voltar",
-            modifier = Modifier.clickable { onBackClick() }
+            style = TextStyle(
+                color = Color.Blue,
+                fontSize = 16.sp,
+                fontWeight = FontWeight.Bold
+            ),
+            modifier = Modifier
+                .clickable { onBackClick() }
+                .padding(8.dp)
         )
     }
 }
